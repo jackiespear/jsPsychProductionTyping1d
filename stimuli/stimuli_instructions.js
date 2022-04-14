f1b8550e57061d9343fb3499605b1cacb4501616
Jackie Spear <jackiespear44@gmail.com>
2022-04-14 17:21:11 -0500

    Adding experiment
    Updated instructions, study and test words. 40 study words and 80 test words

---
+++ stimuli/stimuli_instructions.js
@@ -0,0 +1,6 @@
+study_instructions = [
+{"study_instruction":"<p style='font-size: 28px;'>PRODUCE</p>","data":{"type":"old","category":"music","word":"PRODUCE"}},
+{"study_instruction":"<p style='font-size: 28px;'>PRODUCE</p>","data":{"type":"old","category":"music","word":"PRODUCE"}},
+{"study_instruction":"<p style='font-size: 28px;'>READ</p>","data":{"type":"old","category":"music","word":"READ"}},
+{"study_instruction":"<p style='font-size: 28px;'>READ</p>","data":{"type":"old","category":"music","word":"READ"}},
+]
